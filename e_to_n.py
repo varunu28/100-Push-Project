cccfaa554f06ec0d7828b9fd6018ee320a9099ce
Varun Upadhyay <varun.u28@gmail.com>
2015-11-06 07:55:01 +0530

    Project: 2

---
+++ e_to_n.py
@@ -0,0 +1,13 @@
+from math import e
+
+def e_to_n(n):  #Function to return the value of e limited to n decimal places
+    return '%.*f' % (n, e)
+
+while True:
+    print "Enter a precision factor between 1 and 50"  # A limit between 1 and 50 for inputs
+    n = input()
+    if n>=1 and n<=50:
+        print e_to_n(n)
+        break
+
+
